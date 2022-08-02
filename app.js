3810547a1b9c1b56b220e3a9fd4c0118aeee1d3a
oliverbalog <asperant8@gmail.com>
2022-08-02 16:52:53 +0200

    port change

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const nodemailer = require("nodemailer");
 var cors = require("cors");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cors());
